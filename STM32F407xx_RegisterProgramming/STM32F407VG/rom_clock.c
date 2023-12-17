995690f9b8433c2bbbbc9f90068fa5794371ff3c
Pyae Soan Aung <server01.psa1981@gmail.com>
2023-12-17 06:51:30 +0630

    mco2 output ok, but need power tuning

--- STM32F407xx_RegisterProgramming/STM32F407VG/rom_clock.c
+++ STM32F407xx_RegisterProgramming/STM32F407VG/rom_clock.c
@@ -1,28 +1,12 @@
 #include "rom_clock.h"
 
-void ___ROM_CLOCK_MEASURE_FROM_PA8_MCO1(void)
+// power ပိုင်း လေ့လာပြီး rcc source code အချို့ ကို ပြင်ရန် 
+
+void ___ROM_CLOCK_INIT(void) 
 {
-	// သူက  MCU ရဲ့  clock output ဖြစ် ပါတယ်။ ဒီဖန်ရှင်ကို clock setup မလုပ်ခင် run ရပါမယ်။
-
-	// သူသည် MCO1 clock output 1 ဖြစ်ပါတယ်။
-	// clock source သည်  
-	// HSI ဆိုရင် 0<<21 ,  
-	// LSE ဆိုရင် 1<<21 , 
-	// HSE ဆိုရင် 2<<21 , 
-	// PLL ဆိုရင် 3<<21 ဖြစ်ပြီး PLL ရွေး ထားသည်။
-	//--> RCC->CFGR |= 3<<21;
-
-	// သူသည် prescaler ဖြစ်ပြီး 
-	// 0 ဆို no division, 
-	// 100 ဆို divide by 2, 
-	// 101 ဆို divide by 3, 
-	// 110 ဆို divide by 4, 
-	// 111 ဆို divide by 5 ဖြစ်ပြီး divide by 4 ရွေးထားသည်။ 168/4 = 42MHz 
-	// MCO1 PRE
-	//--> RCC->CFGR |= 6<<24;
+	___ROM_USE_PLL_CLOCK();
+	//test_var *= 2.0;
 }
-
-
 
 void ___ROM_CLOCK_MEASURE_FROM_PC9_MCO2(void)
 {
@@ -42,29 +26,21 @@ void ___ROM_CLOCK_MEASURE_FROM_PC9_MCO2(void)
 	// 110 ဆို divide by 4, ( 42MHz )
 	// 111 ဆို divide by 5  ( 33.6MHz) 
 	// ယခု 110 ရွေးထားသည်။ 168/4 = 42 MHz 
-	RCC->CFGR &= ~(7u<<27);
-	RCC->CFGR |= 6u<<27;
+	RCC->CFGR &= ~(7u<<27); // clear
+	RCC->CFGR |= 6u<<27;    // 7(33MHz) , 6(42MHz), [ 5(56MHz) --> can't generate ]
+	
 }
 
-
-
-// Default အားဖြင့် STM32 MCU တွေသည် HSI ကို အသုံးပြုမှာ ဖြစ်ပြီး HSE သို့မဟုတ် PLL သုံးရန် အောက်ပါ function တစ်ခုခုကို ရွေးပါ။
-void ___ROM_USE_HSE_CLOCK(void)
+void ___ROM_CLOCK_MEASURE_FROM_PC9_GPIO(void)
 {
-	// HSEON bit
-	RCC->CR |= ( 1 << 16);
-
-	// HSERDY bit ( clock ကို stable ဖြစ်စေရန် clock cycle အနည်းငယ်စောင့်ပြီး ready bit ကို စောင့်သည်။ )
-	while( ! ( RCC->CR & (1<<17) ) );
-
-	// SW0, SW1 bit များ ကို ကိုင်ပြီး system clock ကို hse အသုံးပြုခိုင်းသည်။
-	RCC->CFGR &= ~(0x3U); // bit 0 နဲ့ bit 1 ကို 0 ဖြစ်စေ သည်။
-	RCC->CFGR |= 0X01;     // 01 က HSE  
-
-	// ခုကစပြီး MCU သည်  SYSCLK အဖြစ် HSE ကို အသုံးပြုပါသည်။
+	// clock output //
+	
+	// pc9 alternate function 
+	GPIOC->MODER |= 2<<18;
+	
+	// pc9 100 MHz
+	GPIOC->OSPEEDR |= 3<<18;
 }
-
-
 
 void ___ROM_USE_PLL_CLOCK(void)
 {
@@ -112,23 +88,80 @@ void ___ROM_USE_PLL_CLOCK(void)
 	RCC->CFGR &= ~( 7U << 13 );
 	RCC->CFGR |= ( 4U << 13 );
 	
-	// MCO2
-	___ROM_CLOCK_MEASURE_FROM_PC9_MCO2();
+	#ifdef USE_PC9_AS_CLOCK_OUTPUT
+		// MCO2
+		___ROM_CLOCK_MEASURE_FROM_PC9_MCO2();
+	#endif
 	
 	/* Power Interface Clock Enable */
 	RCC->APB1ENR |= 1<<28;
 	
 	/* System configuration controller Clock Enable */
 	RCC->APB2ENR |= 1<<14;
+	
+	// VOS regulator
+	PWR->CSR |= 1<<14;
+	
+	// FLASH data cache , instruction cache , prefetch enable
+	FLASH->ACR |= 1<<8 | 1<<9 | 1<<10; 
+	// latency 5 wait states
+	FLASH->ACR &= ~(7U<<0);
+	FLASH->ACR |= 5u<<0;
+	
+	// AHB
+	RCC->AHB1ENR |= 1 << 2 		// GPIOC
+								| 1 << 0 		// GPIOA
+								| 1 << 7 		// GPIOH
+								| 1 << 20;	// DMA1
+	
 	// ခုကစပြီး MCU သည်  SYSCLK အဖြစ် PLL ကို အသုံးပြုပြီး full speed 168MHz ဖြစ်သည်။
 	// PLLRDY bit
 	while( ! ( RCC->CR & (1<<25) ) );
+	
+	#ifdef USE_PC9_AS_CLOCK_OUTPUT
+	___ROM_CLOCK_MEASURE_FROM_PC9_GPIO();
+	#endif
 }
 
 
-void ___ROM_CLOCK_INIT(void) 
+// Default အားဖြင့် STM32 MCU တွေသည် HSI ကို အသုံးပြုမှာ ဖြစ်ပြီး HSE သို့မဟုတ် PLL သုံးရန် အောက်ပါ function တစ်ခုခုကို ရွေးပါ။
+void ___ROM_USE_HSE_CLOCK(void)
 {
-	___ROM_USE_PLL_CLOCK();
-	//___ROM_CLOCK_MEASURE_FROM_PC9_MCO2();
-	//test_var *= 2.0;
+	// HSEON bit
+	RCC->CR |= ( 1 << 16);
+
+	// HSERDY bit ( clock ကို stable ဖြစ်စေရန် clock cycle အနည်းငယ်စောင့်ပြီး ready bit ကို စောင့်သည်။ )
+	while( ! ( RCC->CR & (1<<17) ) );
+
+	// SW0, SW1 bit များ ကို ကိုင်ပြီး system clock ကို hse အသုံးပြုခိုင်းသည်။
+	RCC->CFGR &= ~(0x3U); // bit 0 နဲ့ bit 1 ကို 0 ဖြစ်စေ သည်။
+	RCC->CFGR |= 0X01;     // 01 က HSE  
+
+	// ခုကစပြီး MCU သည်  SYSCLK အဖြစ် HSE ကို အသုံးပြုပါသည်။
 }
+
+
+/*
+void ___ROM_CLOCK_MEASURE_FROM_PA8_MCO1(void)
+{
+	// သူက  MCU ရဲ့  clock output ဖြစ် ပါတယ်။ ဒီဖန်ရှင်ကို clock setup မလုပ်ခင် run ရပါမယ်။
+
+	// သူသည် MCO1 clock output 1 ဖြစ်ပါတယ်။
+	// clock source သည်  
+	// HSI ဆိုရင် 0<<21 ,  
+	// LSE ဆိုရင် 1<<21 , 
+	// HSE ဆိုရင် 2<<21 , 
+	// PLL ဆိုရင် 3<<21 ဖြစ်ပြီး PLL ရွေး ထားသည်။
+	//--> RCC->CFGR |= 3<<21;
+
+	// သူသည် prescaler ဖြစ်ပြီး 
+	// 0 ဆို no division, 
+	// 100 ဆို divide by 2, 
+	// 101 ဆို divide by 3, 
+	// 110 ဆို divide by 4, 
+	// 111 ဆို divide by 5 ဖြစ်ပြီး divide by 4 ရွေးထားသည်။ 168/4 = 42MHz 
+	// MCO1 PRE
+	//--> RCC->CFGR |= 6<<24;
+}
+
+*/
--- STM32F407xx_RegisterProgramming/STM32F407VG/rom_clock.h
+++ STM32F407xx_RegisterProgramming/STM32F407VG/rom_clock.h
@@ -7,10 +7,16 @@
 
 #include "variables.h"
 
-void ___ROM_CLOCK_MEASURE_FROM_PA8_MCO1(void);
-void ___ROM_CLOCK_MEASURE_FROM_PC9_MCO2(void);
-void ___ROM_USE_HSE_CLOCK(void);
-void ___ROM_USE_PLL_CLOCK(void);
+/* pc9 တွင် clock output လိုချင်ပါက အောက်ပါ MACRO ကို  comment ဖြုတ်ပါ။ */
+#define USE_PC9_AS_CLOCK_OUTPUT 1
+
 void ___ROM_CLOCK_INIT(void);
+void ___ROM_CLOCK_MEASURE_FROM_PC9_MCO2(void);
+void ___ROM_CLOCK_MEASURE_FROM_PC9_GPIO(void);
+void ___ROM_USE_PLL_CLOCK(void);
+
+void ___ROM_CLOCK_MEASURE_FROM_PA8_MCO1(void);
+void ___ROM_USE_HSE_CLOCK(void);
+
 
 #endif
